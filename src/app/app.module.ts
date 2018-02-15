77bddad78bcb6d04a8534688e60af79b0e7eb8dd
Steve Wilson <wilson353@gmail.com>
2018-02-14 18:20:59 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { ModuleWithProviders, NgModule }  from '@angular/core';
+import { RouterModule, Routes }           from '@angular/router';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent }     from './app.component';
+import { StorageComponent } from './storage/storage.component';
+import {
+  HeaderComponent
+} from './shared';
 
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([
+  {
+    path: 'Storage',
+    component: StorageComponent
+  }
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StorageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
